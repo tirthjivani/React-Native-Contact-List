a7f348494d1b2a6f72d898b01300ffbe72c9fb19
tirthjivani <tirthjivani17@gmail.com>
2020-06-27 01:55:28 +0530

    mostly updated

--- AddContactForm.js
+++ AddContactForm.js
@@ -1,82 +1,97 @@
-import React from "react";
-import { Button, StyleSheet, TextInput, View } from "react-native";
-import Constants from "expo-constants";
+import React from 'react'
+import {Button, KeyboardAvoidingView, StyleSheet, TextInput, View} from 'react-native'
+import {Constants} from 'expo'
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: '#fff',
     paddingTop: Constants.statusBarHeight,
   },
   input: {
     borderWidth: 1,
-    borderColor: "black",
+    borderColor: 'black',
     minWidth: 100,
-    marginTop: 10,
+    marginTop: 20,
     marginHorizontal: 20,
     paddingHorizontal: 10,
     paddingVertical: 5,
     borderRadius: 3,
   },
-  buttonadd: {
-    marginTop: 20,
-  },
-});
+})
 
 export default class AddContactForm extends React.Component {
   state = {
-    name: "",
-    phone: "",
+    name: '',
+    phone: '',
     isFormValid: false,
-  };
+  }
 
-  handleNameChange = (name) => {
-    this.setState({ name }, this.validateForm);
-  };
-
-  handlePhoneChange = (phone) => {
-    if (+phone >= 0 && phone.length <= 10) {
-      this.setState({ phone }, this.validateForm);
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.name !== prevState.name || this.state.phone !== prevState.phone) {
+      this.validateForm()
     }
-  };
+  }
+
+  getHandler = key => val => {
+    this.setState({[key]: val})
+  }
+
+  handleNameChange = this.getHandler('name') // val => { this.setState({name: val}) }
+  handlePhoneChange = this.getHandler('phone')
+
+    /*
+  handleNameChange = name => {
+    this.setState({name})
+  }
+  */
+
+  handlePhoneChange = phone => {
+    if (+phone >= 0 && phone.length <= 10) {
+      this.setState({phone})
+    }
+  }
 
   validateForm = () => {
-    if (
-      +this.state.phone >= 0 &&
-      this.state.phone.length === 10 &&
-      this.state.name.length >= 3
-    ) {
-      return this.setState({ isFormValid: true });
+    console.log(this.state)
+    const names = this.state.name.split(' ')
+    if (+this.state.phone >= 0 && this.state.phone.length === 10 && names.length >= 2 && names[0] && names[1]) {
+      this.setState({isFormValid: true})
+    } else {
+      this.setState({isFormValid: false})
     }
-    return this.setState({ isFormValid: false });
-  };
+  }
+
+  validateForm2 = () => {
+    if (+this.state.phone >= 0 && this.state.phone.length === 10 && this.state.name.length >= 3) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+
   handleSubmit = () => {
-    this.props.onSubmit(this.state);
-  };
+    this.props.onSubmit(this.state)
+  }
 
   render() {
     return (
-      <View style={styles.container}>
+      <KeyboardAvoidingView behavior="padding" style={styles.container}>
         <TextInput
           style={styles.input}
           value={this.state.name}
-          onChangeText={this.handleNameChange}
+          onChangeText={this.getHandler('name')}
           placeholder="Name"
         />
         <TextInput
           keyboardType="numeric"
           style={styles.input}
           value={this.state.phone}
-          onChangeText={this.handlePhoneChange}
+          onChangeText={this.getHandler('phone')}
           placeholder="Phone"
         />
-        <Button
-          style={styles.buttonadd}
-          title="Submit"
-          onPress={this.handleSubmit}
-          disabled={!this.state.isFormValid}
-        />
-      </View>
-    );
+        <Button title="Submit" onPress={this.handleSubmit} disabled={!this.state.isFormValid} />
+      </KeyboardAvoidingView>
+    )
   }
 }
--- App.js
+++ App.js
@@ -1,83 +1,52 @@
-import React from "react";
-import {
-  Button,
-  FlatList,
-  ScrollView,
-  StyleSheet,
-  Text,
-  View,
-} from "react-native";
-import Constants from "expo-constants";
+import React from 'react';
+import { Button, FlatList, ScrollView, StyleSheet, Text, View } from 'react-native';
+import {Constants} from 'expo'
 
-import contacts, { compareNames } from "./contacts";
-import ScrollViewContacts from "./ScrollViewContacts";
-import FlatListContacts from "./FlatListContacts";
-import SectionListContacts from "./SectionListContacts";
-import AddContactForm from "./AddContactForm";
+import contacts, {compareNames} from './contacts'
+import ScrollViewContacts from './ScrollViewContacts'
+import FlatListContacts from './FlatListContacts'
+import SectionListContacts from './SectionListContacts'
+import AddContactForm from './AddContactForm'
 
 export default class App extends React.Component {
   state = {
     showContacts: true,
     showForm: false,
     contacts: contacts,
-  };
+  }
 
-  addContact = (newContact) => {
-    this.setState((prevState) => ({
-      contacts: [...prevState.contacts, newContact],
-      showForm: false,
-    }));
-  };
+  addContact = newContact => {
+    this.setState(prevState => ({showForm: false, contacts: [...prevState.contacts, newContact]}))
+  }
+
   toggleContacts = () => {
-    this.setState((prevState) => ({ showContacts: !prevState.showContacts }));
-  };
+    this.setState(prevState => ({showContacts: !prevState.showContacts}))
+  }
 
-  // This is unused in the end
   sort = () => {
-    // sort is a built-in function of every JS array, we can pass a function
-    this.setState((prevState) => ({
-      contacts: prevState.contacts.sort(compareNames),
-    }));
-    // to force "creating" new array => to cause "immutability"
-    // so that the FlatList will notice the array is changed
-    // this.setState(prevState => ({contacts: [...prevState.contacts].sort(compareNames)}))
-  };
+    this.setState(prevState => ({contacts: prevState.contacts.sort(compareNames)}))
+  }
 
   showForm = () => {
-    this.setState({ showForm: true });
-  };
+    this.setState({showForm: true})
+  }
 
   render() {
-    if (this.state.showForm)
-      return <AddContactForm onSubmit={this.addContact} />;
+    if (this.state.showForm) return <AddContactForm onSubmit={this.addContact} />
     return (
       <View style={styles.container}>
-        <Button
-          title="toggle contacts"
-          onPress={this.toggleContacts}
-          style={styles.buttonotggle}
-        />
+        <Button title="toggle contacts" onPress={this.toggleContacts} />
         <Button title="add contact" onPress={this.showForm} />
-        {this.state.showContacts && (
-          <SectionListContacts contacts={this.state.contacts} />
-        )}
+        {this.state.showContacts && <SectionListContacts contacts={this.state.contacts} />}
       </View>
     );
-    // or we can also do something like this
-    // {this.state.showContacts ? <ContactsComponent /> : null}
-    // or just use && like that (if first thing is true then return next; or just return false)
-    // null and false in react rendering are the same
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: '#fff',
     paddingTop: Constants.statusBarHeight,
-  },
-  buttonotggle: {
-    width: "30px",
-    margin: "10px",
   },
 });
